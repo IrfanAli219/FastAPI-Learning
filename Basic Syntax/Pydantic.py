0ad48987f6c378bb71f55eb7f32dc16ffb40d73a
IrfanAli219 <irfan14050@gmail.com>
2026-07-25 16:23:10 +0500

    Updated files

--- Basic Syntax/Pydantic.py
+++ Basic Syntax/Pydantic.py
@@ -1,12 +1,20 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 
-
 app = FastAPI()
 
+class Numbers(BaseModel):
+    a: int
+    b: int
 
-@app.get("/hello")
-def hello():
+@app.post("/add")
+def add(numbers: Numbers):
+
+    result = numbers.a + numbers.b
+
     return {
-        "Hello":"Irfan"
+        "result": result
     }
+
+import pydantic
+print(pydantic.__version__)
